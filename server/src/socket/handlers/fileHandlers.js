b1fcb84795d73febed5649689f6063603a1e4caa
Saksham Panghal <saksham.panghaal@gmail.com>
2026-06-01 18:45:06 +0530

    Refactor fileHandlers to improve code formatting and consistency

--- server/src/socket/handlers/fileHandlers.js
+++ server/src/socket/handlers/fileHandlers.js
@@ -4,17 +4,39 @@ import detectLanguage from "../../utils/detectLanguage.js";
 export default function fileHandlers(socket, io){
 
   socket.on("create-file", ({ roomId, name }) => {
-      const room = activeRooms[roomId];
-      if (!room) return;
+    const room = activeRooms[roomId];
+    if (!room) return;
       
-      room.files[name] = {
-        content: "",
-        language: detectLanguage(name),
-      };
+    room.files[name] = {
+      content: "",
+      language: detectLanguage(name),
+    };
       
-      io.to(roomId).emit("file-created", {
-        name,
-        language: detectLanguage(name),
-      });
+    io.to(roomId).emit("file-created", {
+      name,
+      language: detectLanguage(name),
     });
+  });
+
+  socket.on("rename-file", ({ roomId, oldName, newName }) => {
+    const room = activeRooms[roomId];
+    if (!room) return;
+
+    room.files[newName] = room.files[oldName];
+    delete room.files[oldName];
+
+    io.to(roomId).emit("file-renamed", {
+        oldName,
+        newName,
+    });
+  });
+  
+
+  socket.on("delete-file", ({ roomId, name }) => {
+    const room = activeRooms[roomId];
+    if (!room) return;
+
+    delete room.files[name];
+    io.to(roomId).emit("file-deleted", {name,});
+  });
 }
