0c4797f3b7fa38df2349cec9c556e20bcf6f6a69
Saksham Panghal <saksham.panghaal@gmail.com>
2026-06-09 22:01:48 +0530

    Update socket connection port in client and server configurations

--- server/src/index.js
+++ server/src/index.js
@@ -34,7 +34,7 @@ registerSocketHandlers(io);
 
 startAutosaveService();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
